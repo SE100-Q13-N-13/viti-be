1ec348c799e1b2a250a813d806eb2b4db1369ab2
KhoiLuu143 <khoiluub143@gmail.com>
2025-11-20 20:55:43 +0700

    Initial commit: Setup Spring Boot project structure

---
+++ src/main/java/com/example/viti_be/VitiBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.viti_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitiBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitiBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/viti_be/VitiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.viti_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitiBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
